2ae8bbf243a8b28a64c071e2ab9dd0bb12ea9fca
fenfen22 <fenfenliu021@gmail.com>
2025-10-23 15:49:50 +0000

    Updated code

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
+// main.go
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func pingHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		fmt.Fprint(w, "pong")
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/ping", pingHandler)
-
-	fmt.Println("Server running on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+    fmt.Println("Hello from Yocto Go app!")
 }
